a3110a2d37b845f3d86b3cc4329d30b39edf6b59
Yash Mishra <2018cscloudyash6851@poornima.edu.in>
2025-03-17 09:40:49 +0530

    Add app startup

---
+++ __tests__/App.test.tsx
@@ -0,0 +1,6 @@
+import {render} from "@testing-library/react-native"
+import App from "../App"
+test('Snapshot', () => {
+     const { toJSON } = render(<App />)
+     expect(toJSON()).toMatchSnapshot()
+ })
